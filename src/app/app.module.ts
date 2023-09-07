fa410cce437e94567ce69dbe2d2f6f968953b015
KRISHNA-PRAVESH <krishnapravesh.hj@gmail.com>
2023-09-07 17:33:27 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+
+import { FormsModule } from '@angular/forms'; 
+
+
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    MatSlideToggleModule,
+    
+
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  firebaseConfig: {
+    projectId: 'sih-project-e7dcb',
+    appId: '1:124611971190:web:ec4e7b3ee3a4f67a067757',
+    storageBucket: 'sih-project-e7dcb.appspot.com',
+    apiKey: 'AIzaSyDeamtxRzluHsSeDPj30bq-uwN1XE7Jj3g',
+    authDomain: 'sih-project-e7dcb.firebaseapp.com',
+    messagingSenderId: '124611971190',
+    measurementId: 'G-964QPP9R9R',
+  },
+};
